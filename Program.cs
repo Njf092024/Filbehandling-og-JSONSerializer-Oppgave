dd817c363e08836743daa77aa3c463ad4609ddd5
Njf092024 <Njf092024@gmail.com>
2024-10-28 09:20:08 +0100

    feat: add age input logic

--- Program.cs
+++ Program.cs
@@ -11,7 +11,14 @@ class Program
         {
             Console.WriteLine("what is your name?");
             string? name = Console.ReadLine();
-            
+            Console.WriteLine("How old are you?");
+            string? ageInput = Console.ReadLine();
+            int age;
+            while(!int.TryParse(ageInput, out age))
+            {
+                Console.WriteLine("There was error with the data submitted, please input your age using numbers.");
+                ageInput = Console.ReadLine();
+            }
         }
         catch(Exception exception)
         {
